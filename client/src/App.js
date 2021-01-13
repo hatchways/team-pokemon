af442613ed261eee7df4db02ae449b5e4204e673
Thomas Sobor <sobor.thomas@yahoo.com>
2021-01-13 08:36:59 +0100

    begin implementing user context

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 
@@ -6,12 +6,29 @@ import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
 
 import "./App.css";
+import { UserContext } from "./context/UserContext";
+
+const axios = require("axios");
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      const userRes = await axios.get("/api/getUser");
+      if (userRes /* add check to see if user value was returned */) {
+        setUser(userRes);
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
+
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <UserContext.Provider value={{ user, setUser }}>
+          <Route path="/" component={LandingPage} />
+        </UserContext.Provider>
       </BrowserRouter>
     </MuiThemeProvider>
   );
