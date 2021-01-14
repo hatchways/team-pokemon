a57489e362bc47e6786f648e9b1e85a9614831c8
Thomas Sobor <sobor.thomas@yahoo.com>
2021-01-14 16:10:11 +0100

    implement context that stores user

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
+
+import { AuthStateContext, AuthDispatchContext } from "./context/AuthContext";
+import { getUser } from "./actions/auth";
+import { initialState, AuthReducer } from "./reducers/auth";
+
+import PrivateRoute from "./routing/PrivateRoute";
 
 import { theme } from "./themes/theme";
 //import LandingPage from "./pages/Landing";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import Profile from "./pages/Profile"; //temporary name
+import Profile from "./pages/Profile"; // temporary name
+import TestingRoute from "./pages/TestingRoute";
 
 import "./App.css";
 
 function App() {
-  //loggedIn -> should come form context
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
 
-  //const [loggedIn, setLoggedIn] = useState(false);
-  const loggedIn = false; //for now, use variable instead of state to avoid error on console
+  // Check if user is logged in when App mounts.
+  useEffect(() => {
+    getUser(dispatch);
+  }, []);
+
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/">
-          {loggedIn ? <Profile /> : <Redirect to="/signup" />}
-        </Route>
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
+        <AuthStateContext.Provider value={state}>
+          <AuthDispatchContext.Provider value={dispatch}>
+            <Route exact path="/">
+              {state.isAuthenticated ? (
+                <Redirect to="/profile" />
+              ) : (
+                <Redirect to="/signup" />
+              )}
+            </Route>
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <PrivateRoute exact path="/profile" component={Profile} />
+            <PrivateRoute exact path="/testingroute" component={TestingRoute} />
+          </AuthDispatchContext.Provider>
+        </AuthStateContext.Provider>
       </BrowserRouter>
     </MuiThemeProvider>
   );
