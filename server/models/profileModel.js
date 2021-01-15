18ae2e82b5070817f014e1db70a854e92089c975
Thomas Sobor <sobor.thomas@yahoo.com>
2021-01-15 18:42:29 +0100

    update profile model to include isSitter flag, phone number, address, and reference to user

--- server/models/profileModel.js
+++ server/models/profileModel.js
@@ -2,27 +2,40 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const profileSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: [true, "Please provide your first name."],
-        validate: [validator.isAlpha, "First name must only contain characters."],
-      },
-      lastName: {
-        type: String,
-        required: [true, "Please provide your last name."],
-        validate: [validator.isAlpha, "Last name must only contain characters."],
-      },
-      gender: {
-          type: String,
-          validate: [validator.isAlpha, "gender must only contain characters."],
-      },
-      birthDate: {
-          type: Date,
-      },
-      description: {
-          type: String,
-          validate: [validator.isAlpha, "description must contain only characters"]
-      }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  isSitter: {
+    type: Boolean,
+    default: false,
+  },
+  firstName: {
+    type: String,
+    required: [true, "Please provide your first name."],
+    validate: [validator.isAlpha, "First name must only contain characters."],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please provide your last name."],
+    validate: [validator.isAlpha, "Last name must only contain characters."],
+  },
+  gender: {
+    type: String,
+    validate: [validator.isAlpha, "Gender must only contain characters."],
+  },
+  birthDate: {
+    type: Date,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
 });
 
 const Profile = mongoose.model("Profile", profileSchema);
