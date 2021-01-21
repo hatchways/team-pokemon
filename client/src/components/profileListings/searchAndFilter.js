df282fdf7d0b0f21d32785dba5e302280f147e21
Qudus <qudusopeyemi95@gmail.com>
2021-01-21 10:55:57 -0500

    prettier code formatting and renaming components
    Renamed components to start with uppercase letter
    
    -use prettier to format code.

--- client/src/components/profileListings/searchAndFilter.js
+++ client/src/components/profileListings/searchAndFilter.js
@@ -1,82 +1,84 @@
-import React from 'react';
+import React from "react";
 
-import { Container, Grid, TextField, Typography } from '@material-ui/core/';
-import { makeStyles } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
+import { Container, Grid, TextField, Typography } from "@material-ui/core/";
+import { makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
 
-const useStyles = makeStyles( theme => ({
-    search: {
-        marginTop: theme.spacing(10),
-    },
-    searchInputs: {
-        margin: theme.spacing(0),
-    },
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center'
-      },
-    dateField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(0),
-        width: 200,
-      },
-      textField: {
-        fontWeight:"bold"
-      }
-}))
-function SearchAndFilter(){
-    const classes = useStyles();
-    return (
-        <Container maxWidth="lg" className={classes.search}>
-                <Typography 
-                    component="h5" 
-                    variant="h5" 
-                    align="center"
-                    style={{fontWeight:"bold", marginBottom: "10px"}}>
-                    Your search results
-                </Typography>
-                {/* Search buttons */}
-                   <div className={classes.searchInputs}>
-                   <Grid container spacing={0} justify="center" alignItems="flex-end">
-                     <Grid item>
-                       <SearchIcon style={{color: '#f04040'}}/>
-                     </Grid>
-                     <Grid item>
-                       <TextField 
-                            id="input-with-icon-grid" 
-                            label="location" 
-                            variant="standard"
-                            className={classes.textField}/>
-                     </Grid>
-                     <Grid item>
-                     <form className={classes.container} noValidate>
-                        <TextField
-                            id="date-picker-inline"
-                            label="Drop off"
-                            type="date"
-                            defaultValue="today"
-                            className={classes.dateField}
-                            InputLabelProps={{
-                            shrink: true,
-                            }}
-                        />
-                        <TextField
-                            id="date"
-                            label="Pickup"
-                            type="date"
-                            defaultValue="today"
-                            className={classes.dateField}
-                            InputLabelProps={{
-                            shrink: true,
-                            }}
-                        />
-                    </form>
-                    </Grid>
-                   </Grid>
-                </div>
-            </Container>
-    )
+const useStyles = makeStyles((theme) => ({
+  search: {
+    marginTop: theme.spacing(10),
+  },
+  searchInputs: {
+    margin: theme.spacing(0),
+  },
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  dateField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(0),
+    width: 200,
+  },
+  textField: {
+    fontWeight: "bold",
+  },
+}));
+function SearchAndFilter() {
+  const classes = useStyles();
+  return (
+    <Container maxWidth='lg' className={classes.search}>
+      <Typography
+        component='h5'
+        variant='h5'
+        align='center'
+        style={{ fontWeight: "bold", marginBottom: "10px" }}
+      >
+        Your search results
+      </Typography>
+      {/* Search buttons */}
+      <div className={classes.searchInputs}>
+        <Grid container spacing={0} justify='center' alignItems='flex-end'>
+          <Grid item>
+            <SearchIcon style={{ color: "#f04040" }} />
+          </Grid>
+          <Grid item>
+            <TextField
+              id='input-with-icon-grid'
+              label='location'
+              variant='standard'
+              className={classes.textField}
+            />
+          </Grid>
+          <Grid item>
+            <form className={classes.container} noValidate>
+              <TextField
+                id='date-picker-inline'
+                label='Drop off'
+                type='date'
+                defaultValue='today'
+                className={classes.dateField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                id='date'
+                label='Pickup'
+                type='date'
+                defaultValue='today'
+                className={classes.dateField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </form>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
+  );
 }
 
 export default SearchAndFilter;
--- client/src/pages/ProfileListings.js
+++ client/src/pages/ProfileListings.js
@@ -1,92 +1,101 @@
-import React , { useState, useEffect } from 'react';
-import axios from 'axios';
-import { CssBaseline, Grid } from '@material-ui/core/';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { CssBaseline, Grid } from "@material-ui/core/";
 
-import { makeStyles } from '@material-ui/core/styles';
-import ProfileCard  from '../components/profileListings/profileCard';
-import SearchAndFilter  from '../components/profileListings/searchAndFilter';
+import { makeStyles } from "@material-ui/core/styles";
+import ProfileCard from "../components/profileListings/ProfileCard";
+import SearchAndFilter from "../components/profileListings/SearchAndFilter";
 
+const useStyles = makeStyles((theme) => ({
+  gridContainer: {
+    marginTop: theme.spacing(3),
+  },
+  errorAndLoading: {
+    textAlign: "center",
+  },
+}));
 
-const useStyles = makeStyles(theme => ({
-      gridContainer: {
-          marginTop: theme.spacing(3),
-      },
-      errorAndLoading: {
-          textAlign:'center'
-      }
-}))
+function ProfileListings() {
+  const classes = useStyles();
+  const url = "/api/profile/";
 
-function ProfileListings(){
-    const classes = useStyles();
-    const url = '/api/profile/'
+  const [sitters, setSitters] = useState({
+    loading: false,
+    data: null,
+    error: undefined,
+  });
 
-    const [sitters, setSitters] = useState({
-        loading: false,
-        data: null,
-        error: undefined
+  useEffect(() => {
+    setSitters({
+      loading: true,
+      data: null,
+      error: undefined,
     });
-    
-    useEffect(() =>{
+    axios
+      .get(url)
+      .then((response) => {
         setSitters({
-            loading: true,
-            data: null,
-            error: undefined,
-        })
-        axios.get(url)
-            .then(response => {
-                setSitters({
-                    loading: false,
-                    data: response.data,
-                    error: undefined
-                })
-            }).catch(error => {
-                setSitters({
-                    loading: false,
-                    data: null,
-                    error: error.message
-                })
-            })
-    }, [url])
+          loading: false,
+          data: response.data,
+          error: undefined,
+        });
+      })
+      .catch((error) => {
+        setSitters({
+          loading: false,
+          data: null,
+          error: error.message,
+        });
+      });
+  }, [url]);
 
-    let content = null;
+  let content = null;
 
-    //set sitters content to display
-    if(sitters.error){ //if error occurs, display error message
-        content = <h1>
-            {sitters.error}
-        </h1>
-    }
-    if(sitters.loading){ // display a loading page while page is requesting data
-        content = <h1>Loading...</h1>
-    }
-    if(sitters.data){ // set profile data to display
-        content = 
-        sitters.data.map((sitter) => 
-            <ProfileCard key = {sitter._id}
-                firstName={sitter.firstName} 
-                lastName={sitter.lastName}
-                profilePicture={sitter.profilePicture}
-                rating={4}
-                description={sitter.description} />
-        )
-    }
+  //set sitters content to display
+  if (sitters.error) {
+    //if error occurs, display error message
+    content = <h1>{sitters.error}</h1>;
+  }
+  if (sitters.loading) {
+    // display a loading page while page is requesting data
+    content = <h1>Loading...</h1>;
+  }
+  if (sitters.data) {
+    // set profile data to display
+    content = sitters.data.map((sitter) => (
+      <ProfileCard
+        key={sitter._id}
+        firstName={sitter.firstName}
+        lastName={sitter.lastName}
+        profilePicture={sitter.profilePicture}
+        rating={4}
+        description={sitter.description}
+      />
+    ));
+  }
 
-    return(
-        <React.Fragment>
-            <CssBaseline />
-            <Grid container direction="column" justify="center" alignItems="center">
-                <Grid item>
-                    <SearchAndFilter />
-                </Grid>
-                <Grid item>
-                    <Grid container direction="row" spacing={3} justify="center" alignItems="flex-start"  className={classes.gridContainer}>
-                    {content}
-                    </Grid>
-                </Grid>
-            </Grid>
-
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Grid container direction='column' justify='center' alignItems='center'>
+        <Grid item>
+          <SearchAndFilter />
+        </Grid>
+        <Grid item>
+          <Grid
+            container
+            direction='row'
+            spacing={3}
+            justify='center'
+            alignItems='flex-start'
+            className={classes.gridContainer}
+          >
+            {content}
+          </Grid>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 }
 
 export default ProfileListings;
