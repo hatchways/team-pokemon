5075945ecbf0b06881743b796ef9e357e23db6a6
Qudus <qudusopeyemi95@gmail.com>
2021-01-26 15:34:22 -0500

    make availability router private

--- server/routes/availabilityRouter.js
+++ server/routes/availabilityRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const availabilityController = require("../controllers/availabilityController");
 const verifyToken = require("../middleware/verifyToken");
 
-//router.use(verifyToken)
+router.use(verifyToken)
 router.post("/:id", availabilityController.add);
 
 module.exports = router;
