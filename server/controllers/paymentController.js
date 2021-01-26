5880ca11143138c03a26695f16d6fbbff1e88a50
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2021-01-25 20:21:17 -0500

    controller async func upd

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,84 +1,97 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const createError = require("http-errors");
 
 //retrieve a customer with POST by searching their email
-exports.getCustomer = async (req, res) => {
-  const { id, name, email } = req.body;
-  const customerInfo = {
-    id: id,
-    name: name,
-    email: email,
-    description: "Dog sitter service customer",
-  };
-  let existingCustomer = await stripe.customers.list({
-    email: email,
-  });
-  if (existingCustomer.data.length > 0) {
-    //customer exists -> retrieve their cards
-    const cards = await stripe.customers.listSources(id, {
-      object: "card",
-      limit: 5,
+exports.getCustomer = async (req, res, next) => {
+  try {
+    const { id, name, email } = req.body;
+    const customerInfo = {
+      id: id,
+      name: name,
+      email: email,
+      description: "Dog sitter service customer",
+    };
+    let existingCustomer = await stripe.customers.list({
+      email: email,
     });
-    const customerCards = await cards.data.map(card => {
-      return {
-        id: card.id,
-        brand: card.brand,
-        month: card.exp_month.toString(),
-        year: card.exp_year.toString(),
-        last4: card.last4,
-        isDefault:
-          existingCustomer.data[0].default_source == card.id ? true : false,
-      };
-    });
-    res.json({ error: false, cards: customerCards });
-  } else {
-    //customer doesn't exist yet -> create new customer
-    await stripe.customers.create(customerInfo, (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      if (data) {
-        res.json({
-          error: false,
-          cards: null,
-        });
-      } else {
-        console.log("Unknown error occured");
-      }
-    });
+    if (existingCustomer.data.length > 0) {
+      //customer exists -> retrieve their cards
+      const cards = await stripe.customers.listSources(id, {
+        object: "card",
+        limit: 5,
+      });
+      const customerCards = await cards.data.map(card => {
+        return {
+          id: card.id,
+          brand: card.brand,
+          month: card.exp_month.toString(),
+          year: card.exp_year.toString(),
+          last4: card.last4,
+          isDefault:
+            existingCustomer.data[0].default_source == card.id ? true : false,
+        };
+      });
+      res.json({ error: false, cards: customerCards });
+    } else {
+      //customer doesn't exist yet -> create new customer
+      await stripe.customers.create(customerInfo, (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        if (data) {
+          res.json({
+            error: false,
+            cards: null,
+          });
+        } else {
+          console.log("Unknown error occured");
+        }
+      });
+    }
+  } catch (err) {
+    next(createError(500, err.message));
   }
 };
 
-exports.addCard = async (req, res) => {
-  const { userId, token } = req.body;
-  const card = await stripe.customers.createSource(userId, {
-    source: token.id,
-  });
-  if (card) {
-    res.json({
-      error: false,
-      data: {
-        id: card.id,
-        brand: card.brand,
-        month: card.exp_month,
-        year: card.exp_year,
-        last4: card.last4,
-      },
+exports.addCard = async (req, res, next) => {
+  try {
+    const { userId, token } = req.body;
+    const card = await stripe.customers.createSource(userId, {
+      source: token.id,
     });
-  } else {
-    res.json({ error: true, data: null });
+    if (card) {
+      res.json({
+        error: false,
+        data: {
+          id: card.id,
+          brand: card.brand,
+          month: card.exp_month,
+          year: card.exp_year,
+          last4: card.last4,
+        },
+      });
+    } else {
+      res.json({ error: true, data: null });
+    }
+    return;
+  } catch (err) {
+    next(createError(500, err.message));
   }
-  return;
 };
 
-exports.updateDefault = async (req, res) => {
-  const { userId, card } = req.body;
-  const customer = await stripe.customers.update(userId, {
-    default_source: card,
-  });
-  if (customer) {
-    res.json({ error: false, data: customer.default_source });
-  } else {
-    res.json({ error: true, data: null });
+exports.updateDefault = async (req, res, next) => {
+  try {
+    const { userId, card } = req.body;
+    const customer = await stripe.customers.update(userId, {
+      default_source: card,
+    });
+    if (customer) {
+      res.json({ error: false, data: customer.default_source });
+    } else {
+      res.json({ error: true, data: null });
+    }
+    return;
+  } catch {
+    next(createError(500, err.message));
   }
-  return;
 };
