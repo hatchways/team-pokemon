c3b0ebc72a4a0c66fabbe022971d67e90d32b092
Thomas Sobor <sobor.thomas@yahoo.com>
2021-01-29 22:16:58 +0100

    add pay request action + reducer

--- client/src/actions/request.js
+++ client/src/actions/request.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { PAY_BOOKING_SUCCESS, PAY_BOOKING_FAILURE } from "./types";
 
-// Update Profile
+// Create Request
 export const createRequest = async (payload) => {
   try {
     const config = {
@@ -10,6 +11,30 @@ export const createRequest = async (payload) => {
     };
     await axios.post("/api/request/", payload, config);
   } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const payRequest = async (dispatch, payload) => {
+  try {
+    const { id, amount } = payload;
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const body = JSON.stringify({ id, amount });
+    await axios.post(`/api/request/${id}/pay`, body, config);
+    dispatch({ type: PAY_BOOKING_SUCCESS, payload: id });
+  } catch (err) {
+    const error = err.response.data.error;
+    console.log(error);
+    if (error) {
+      dispatch({
+        type: PAY_BOOKING_FAILURE,
+        payload: err.response.data.message,
+      });
+    }
     console.log(err.message);
   }
 };
