b6fab9d48df48d645ec1f46144c64f278822049f
Qudus <qudusopeyemi95@gmail.com>
2021-02-04 10:05:22 -0500

    select days from drop down

--- client/src/components/profile/ProfileRequestForm.js
+++ client/src/components/profile/ProfileRequestForm.js
@@ -8,12 +8,17 @@ import {
   TextField,
   Typography,
   makeStyles,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@material-ui/core";
 import {
   AuthDispatchContext,
   AuthStateContext,
 } from "../../context/AuthContext";
 import { createRequest } from "../../actions/request";
+import { eachHourOfInterval, format } from "date-fns";
 
 const useStyles = makeStyles(() => ({
   centerContent: {
@@ -40,7 +45,7 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-function ProfileRequestForm({ sitterId }) {
+function ProfileRequestForm({ sitterId, sitterPrice, sitterAvailability }) {
   const classes = useStyles();
 
   // Get dispatch method and state from auth context
@@ -52,20 +57,10 @@ function ProfileRequestForm({ sitterId }) {
   const todayFormatted = today.toISOString().split("T")[0];
 
   const [requestSending, setRequestSending] = useState(false);
-  const [startDate, setStartDate] = useState(todayFormatted);
-  const [endDate, setEndDate] = useState(todayFormatted);
-  const [startTime, setStartTime] = useState(
-    moment()
-      .add(moment().utcOffset() + 30, "minutes")
-      .startOf("hour")
-      .format("HH:mm")
-  );
-  const [endTime, setEndTime] = useState(
-    moment()
-      .add(moment().utcOffset() + 150, "minutes")
-      .startOf("hour")
-      .format("HH:mm")
-  );
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
   const [requestFormData, setRequestFormData] = useState({
     sitterId: sitterId,
     ownerId: user._id,
@@ -80,6 +75,9 @@ function ProfileRequestForm({ sitterId }) {
       .startOf("hour")
       .format(),
   });
+  const [startHourInterval, setStartHourInterval] = useState([]);
+  const [endHourInterval, seEndhourInterval] = useState([]);
+  const [index, setIndex] = useState();
 
   // Changes 'Send Request' button from spinning to normal when alert pops ups
   useEffect(() => {
@@ -180,11 +178,11 @@ function ProfileRequestForm({ sitterId }) {
 
   return (
     <>
-      <Typography variant="h4" className={classes.hourlyRateHeading}>
-        $14/hr
+      <Typography variant='h4' className={classes.hourlyRateHeading}>
+        ${sitterPrice}/hr
       </Typography>
       <Rating
-        name="simple-controlled"
+        name='simple-controlled'
         value={3}
         readOnly
         className={classes.ratingsAlignment}
@@ -194,22 +192,58 @@ function ProfileRequestForm({ sitterId }) {
           <Typography className={classes.labelStyles}>DROP OFF</Typography>
         </Box>
         <Box className={classes.inputGap}>
+          <FormControl variant='outlined'>
+            <InputLabel id='dropoff-date'>date</InputLabel>
+            <Select
+              labelId='demo-simple-select-outlined-label'
+              id='demo-simple-select-outlined'
+              value={startDate}
+              onChange={(e) => handleStartDateChange(e)}
+              label='Drop Off Date'
+            >
+              <MenuItem value=''>
+                <em>None</em>
+              </MenuItem>
+              {sitterAvailability.map((date, key) => (
+                <MenuItem
+                  key={key}
+                  value={format(new Date(date.start), "yyyy-MM-dd")}
+                >
+                  {format(new Date(date.start), "dd MMMM, yyyy")}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl variant='outlined'>
+            <InputLabel id='dropoff-date'>time</InputLabel>
+            <Select
+              labelId='demo-simple-select-outlined-label'
+              id='demo-simple-select-outlined'
+              value={startTime}
+              onChange={(e) => handleStartTimeChange(e)}
+              label='Drop Off Time'
+            >
+              <MenuItem value=''>
+                <em>None</em>
+              </MenuItem>
+            </Select>
+          </FormControl>
           <TextField
             InputProps={{
               inputProps: { min: todayFormatted },
             }}
-            type="date"
+            type='date'
             value={startDate}
             onChange={(e) => handleStartDateChange(e)}
-            variant="outlined"
+            variant='outlined'
           ></TextField>
           <TextField
             inputProps={{
               step: 300, // 5 min
             }}
-            type="time"
+            type='time'
             value={startTime}
-            variant="outlined"
+            variant='outlined'
             className={classes.textField + " " + classes.timeInputWidth}
             onChange={(e) => {
               handleStartTimeChange(e);
@@ -222,23 +256,62 @@ function ProfileRequestForm({ sitterId }) {
           <Typography className={classes.labelStyles}>PICK UP</Typography>
         </Box>
         <Box className={classes.endDateAlign}>
+          <FormControl variant='outlined'>
+            <InputLabel id='pickup-date'>date</InputLabel>
+            <Select
+              labelId='demo-simple-select-outlined-label'
+              id='demo-simple-select-outlined'
+              value={endDate}
+              onChange={(e) => handleEndDateChange(e)}
+              label='Age'
+            >
+              <MenuItem value=''>
+                <em>None</em>
+              </MenuItem>
+              {sitterAvailability.map((date) => (
+                <MenuItem
+                  key={date._id}
+                  value={format(new Date(date.start), "yyyy-MM-dd")}
+                >
+                  {format(new Date(date.start), "dd MMMM, yyyy")}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl variant='outlined'>
+            <InputLabel id='pickup-date'>time</InputLabel>
+            <Select
+              labelId='demo-simple-select-outlined-label'
+              id='demo-simple-select-outlined'
+              value={endTime}
+              onChange={(e) => handleEndDateChange(e)}
+              label='endTime'
+            >
+              <MenuItem value=''>
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
           <TextField
             InputProps={{
               inputProps: { min: startDate },
             }}
-            type="date"
-            variant="outlined"
-            value={endDate}
-            onChange={(e) => handleEndDateChange(e)}
+            type='date'
+            variant='outlined'
+            value={endTime}
+            onChange={(e) => handleEndTimeChange(e)}
           ></TextField>
           <TextField
             inputProps={{
               step: 300, // 5 min
             }}
-            type="time"
+            type='time'
             value={endTime}
             className={classes.textField + " " + classes.timeInputWidth}
-            variant="outlined"
+            variant='outlined'
             onChange={(e) => {
               handleEndTimeChange(e);
             }}
@@ -246,15 +319,15 @@ function ProfileRequestForm({ sitterId }) {
         </Box>
       </Box>
       <Button
-        color="primary"
-        variant="contained"
-        type="submit"
-        size="large"
+        color='primary'
+        variant='contained'
+        type='submit'
+        size='large'
         className={classes.buttonStyles}
         onClick={(e) => handleSubmit(e)}
       >
         {requestSending ? (
-          <CircularProgress color="white" size={20} />
+          <CircularProgress color='white' size={20} />
         ) : (
           `SEND REQUEST`
         )}
